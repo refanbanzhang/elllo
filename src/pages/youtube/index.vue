ca623d1c40f29b1e52d5851d089a682de5d6edf2
refanbanzhang <gaoqiang19514@163.com>
2024-10-25 07:59:48 +0800

    refactor: improve playback status logic

--- src/pages/youtube/index.vue
+++ src/pages/youtube/index.vue
@@ -1,12 +1,28 @@
 <script setup lang="ts">
-import { ref } from "vue"
+import { computed, ref } from "vue"
 
 const audio = ref<HTMLAudioElement>(new Audio())
-const currentIndex = ref<number>(0)
+const currentIndex = ref<number>()
 const items = ref<string[]>([
   "https://s3-us-west-1.amazonaws.com/elllo-audio/mixer-001-150/004-MXR-cinema.mp3",
   "https://s3-us-west-1.amazonaws.com/elllo-audio/mixer-001-150/003-MXR-catsdogs.mp3",
 ])
+
+const currentTime = ref<number>(audio.value.currentTime)
+const isPaused = ref<boolean>(audio.value.paused)
+const isPlaying = computed<boolean>(() => !isPaused.value)
+
+audio.value.addEventListener("play", () => {
+  isPaused.value = false
+})
+
+audio.value.addEventListener("pause", () => {
+  isPaused.value = true
+})
+
+audio.value.addEventListener("timeupdate", () => {
+  currentTime.value = audio.value.currentTime
+})
 
 audio.value.addEventListener("ended", () => {
   console.log("音频播放结束")
@@ -16,30 +32,21 @@ audio.value.addEventListener("error", () => {
   console.log("音频播放错误")
 })
 
+
 const play = (index: number) => {
-  const isCurrentAudio = currentIndex.value === index
-  const isPaused = audio.value.paused && audio.value.currentTime > 0
-  const isPlaying = !audio.value.paused && audio.value.currentTime > 0
-
-  // 情况1：暂停正在播放的歌曲
-  if (isCurrentAudio && isPlaying) {
-    audio.value.pause()
-    return
-  }
-
-  // 情况2：播放暂停的歌曲
-  if (isCurrentAudio && isPaused) {
-    audio.value.play()
-    return
-  }
-
-  // 情况3：播放指定歌曲
-  audio.value.pause()
+  pause()
   currentIndex.value = index
   audio.value.src = items.value[index]
   audio.value.play()
 }
 
+const pause = () => {
+  audio.value.pause()
+}
+
+const resume = () => {
+  audio.value.play()
+}
 </script>
 
 <template>
@@ -49,8 +56,23 @@ const play = (index: number) => {
       v-for="(_, index) in items"
       :key="index"
     >
-      <button @click="play(index)">
-        播放{{ index }}
+      <button
+        @click="pause"
+        v-if="currentIndex === index && isPlaying"
+      >
+        暂停
+      </button>
+      <button
+        @click="resume"
+        v-else-if="currentIndex === index && isPaused"
+      >
+        播放
+      </button>
+      <button
+        v-else
+        @click="play(index)"
+      >
+        播放
       </button>
     </div>
   </div>
