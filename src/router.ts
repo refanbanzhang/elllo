172bbe923ee58427bd2dabc35d5af0f9bfc8ea6c
refanbanzhang <gaoqiang19514@163.com>
2024-10-25 08:02:04 +0800

    chore: add playground

--- src/router.ts
+++ src/router.ts
@@ -2,10 +2,12 @@ import { createWebHashHistory, createRouter } from "vue-router"
 
 import elllo from "./pages/elllo/index.vue"
 import youtube from "./pages/youtube/index.vue"
+import test from "./pages/test/index.vue"
 
 const routes = [
   { path: "/", component: elllo },
   { path: "/youtube", component: youtube },
+  { path: "/test", component: test },
 ]
 
 const router = createRouter({
