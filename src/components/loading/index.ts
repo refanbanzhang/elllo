e2dc5158a887093e9ca2214df9289e0be37af6ff
refanbanzhang <gaoqiang19514@163.com>
2025-01-26 08:00:04 +0800

    feat: 维护loading实例

--- src/components/loading/index.ts
+++ src/components/loading/index.ts
@@ -1,20 +1,21 @@
 import { createApp } from "vue"
 import Loading from "./index.vue"
 
+const loadingInstances: HTMLElement[] = []
+
 const showLoading = () => {
-  const app = document.querySelector("#app")
   const container = document.createElement("div")
   container.classList.add("loading")
   const loadingApp = createApp(Loading)
   loadingApp.mount(container)
-  app?.appendChild(container)
+  document.body.appendChild(container)
+  loadingInstances.push(container)
 }
 
 const hideLoading = () => {
-  const app = document.querySelector("#app")
-  const loading = app?.querySelector(".loading")
-  if (loading) {
-    loading.remove()
+  const lastLoading = loadingInstances.pop()
+  if (lastLoading) {
+    lastLoading.remove()
   }
 }
 
