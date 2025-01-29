cb35b09ee9b49823af765f0e0d47fa30147d0469
refanbanzhang <gaoqiang19514@163.com>
2025-01-29 17:54:50 +0800

    refactor: revert code

--- src/pages/lessons/index.vue
+++ src/pages/lessons/index.vue
@@ -17,6 +17,10 @@ const { lockPage, unlockPage } = usePageLocker()
 const visible = ref(false)
 const currentLesson = ref<LessonType | null>(null)
 
+const onUpdateLesson = (lesson: LessonType) => {
+  currentLesson.value = lesson
+}
+
 const onOpenPopup = (lesson: LessonType) => {
   currentLesson.value = lesson
   visible.value = true
@@ -93,6 +97,7 @@ onUnmounted(() => {
       <Lesson
         v-if="currentLesson"
         :data="currentLesson"
+        @update="onUpdateLesson"
         @close="onClose"
       />
     </popup>
