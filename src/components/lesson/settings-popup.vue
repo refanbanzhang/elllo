a0c0be9886be964858f4a8fd33081ba93622b774
refanbanzhang <gaoqiang19514@163.com>
2025-02-05 21:32:29 +0800

    feat: enhance settings popup with playback speed display

--- src/components/lesson/settings-popup.vue
+++ src/components/lesson/settings-popup.vue
@@ -4,6 +4,7 @@ import { getProxiedImageUrl } from "@/utils"
 import Popup from "@/components/popup/index.vue"
 import sleepTimerStore from "@/stores/sleep-timer"
 import currentLessonStore from "@/stores/current-lesson"
+import playerStore from "@/stores/player"
 
 interface Props {
   visible: boolean
@@ -16,9 +17,9 @@ const emit = defineEmits<{
   (e: "speed"): void
 }>()
 
+const { speed } = playerStore
 const { remainingTime } = sleepTimerStore
 const { lesson } = currentLessonStore
-
 const imgSrc = computed(() => lesson.value?.img ? getProxiedImageUrl(lesson.value.img) : "")
 
 const onClose = () => {
@@ -75,13 +76,18 @@ const menuItems = computed(() => [
       </div>
       <div class="menus">
         <div
-          class="menu-item"
+          class="menu"
           v-for="(item, index) in menuItems"
           :key="index"
           @click="item.action?.()"
         >
-          {{ item.name }}
-          {{ item.name === "Sleep timer" && remainingTime > 0 ? `${Math.floor(remainingTime / 60)}:${(remainingTime % 60).toString().padStart(2, "0")} left` : "" }}
+          <span>
+            {{ item.name }}
+          </span>
+          <span>
+            {{ item.name === "Sleep timer" && remainingTime > 0 ? `${Math.floor(remainingTime / 60)}:${(remainingTime % 60).toString().padStart(2, "0")}` : "" }}
+            {{ item.name === "Set speed" && speed > 0 ? `${speed}x` : "" }}
+          </span>
         </div>
       </div>
       <div class="settings-close" @click="onClose">close</div>
@@ -93,45 +99,48 @@ const menuItems = computed(() => [
 .settings {
   overflow: auto;
   height: 100%;
-  padding: 20px 40px;
+  padding: 20px 30px;
   color: #fff;
-}
 
-.settings-cover {
-  margin-top: 40px;
-  margin-bottom: 20px;
-  width: 100%;
-}
+  .settings-cover {
+    margin-top: 40px;
+    margin-bottom: 20px;
+    width: 100%;
+  }
 
-.settings-title {
-  margin-bottom: 10px;
-  font-size: 20px;
-  font-weight: 600;
-}
+  .settings-title {
+    margin-bottom: 10px;
+    font-size: 20px;
+    font-weight: 600;
+  }
 
-.settings-lesson-no {
-  margin-bottom: 70px;
-  font-size: 16px;
-  color: #999;
-}
+  .settings-lesson-no {
+    margin-bottom: 70px;
+    font-size: 16px;
+    color: #999;
+  }
 
-.settings-close {
-  position: absolute;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  padding: 20px;
-  box-sizing: border-box;
-  text-align: center;
-  color: #fff;
-  font-size: 18px;
+  .settings-close {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    padding: 20px;
+    box-sizing: border-box;
+    text-align: center;
+    color: #fff;
+    font-size: 18px;
+  }
 }
 
 .menus {
   margin-bottom: 100px;
-}
 
-.menu-item {
-  padding: 15px 0;
+  .menu {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 15px 0;
+  }
 }
 </style>
